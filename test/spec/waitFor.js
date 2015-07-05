aa5c4acf4f01729854bef46663501199204f87bd
André de Jager <andrethehunter@gmail.com>
2015-07-05 22:16:47 +0100

    display visibility test,
    * added .notDisplayable div for visibility testing
    * updated test/spec/waitFor.js
    ** renamed isTrue to checkTrue
    ** added display test for checking display:none visibility

--- test/spec/waitFor.js
+++ test/spec/waitFor.js
@@ -2,11 +2,11 @@ describe('waitFor',function() {
 
     var duration = 10000;
 
-    function isTrue(res) {
+    function checkTrue(res) {
         expect(res).to.be.true;
     };
 
-    function isFalse(res) {
+    function checkFalse(res) {
         expect(res).to.be.false;
     };
 
@@ -15,11 +15,11 @@ describe('waitFor',function() {
         beforeEach(h.setup());
 
         it('should return w/o err after element was enabled', function() {
-            return this.client.waitForEnabled('//html/body/section/input[8]', duration).then(isTrue);
+            return this.client.waitForEnabled('//html/body/section/input[8]', duration).then(checkTrue);
         });
 
         it('(reverse) should return w/o err after element was disabled', function() {
-            return this.client.waitForEnabled('.waitForValueEnabledReverse', duration, true).then(isTrue);
+            return this.client.waitForEnabled('.waitForValueEnabledReverse', duration, true).then(checkTrue);
         });
 
     });
@@ -29,11 +29,11 @@ describe('waitFor',function() {
         beforeEach(h.setup());
 
         it('should return w/o err after element was appended to the DOM', function() {
-            return this.client.waitForExist('//div[text()="Sorry, I\'m late!"]', duration).then(isTrue);
+            return this.client.waitForExist('//div[text()="Sorry, I\'m late!"]', duration).then(checkTrue);
         });
 
         it('(reverse) should return w/o err after element was removed from the DOM', function() {
-            return this.client.waitForExist('.goAway', duration, true).then(isTrue);
+            return this.client.waitForExist('.goAway', duration, true).then(checkTrue);
         });
 
     });
@@ -43,11 +43,11 @@ describe('waitFor',function() {
         beforeEach(h.setup());
 
         it('should return w/o err after element was selected', function() {
-            return this.client.waitForSelected('//*[@id="selectbox"]/option[3]', duration).then(isTrue);
+            return this.client.waitForSelected('//*[@id="selectbox"]/option[3]', duration).then(checkTrue);
         });
 
         it('(reverse) should return w/o err after element was unselected', function() {
-            return this.client.waitForSelected('.option2', duration, true).then(isTrue);
+            return this.client.waitForSelected('.option2', duration, true).then(checkTrue);
         });
 
     });
@@ -57,11 +57,11 @@ describe('waitFor',function() {
         beforeEach(h.setup());
 
         it('should return w/o err after element got a text/content', function() {
-            return this.client.waitForText('//*[contains(@class, "sometextlater")]', duration).then(isTrue);
+            return this.client.waitForText('//*[contains(@class, "sometextlater")]', duration).then(checkTrue);
         });
 
         it('(reverse) should return w/o err after text/content element was removed', function() {
-            return this.client.waitForText('.sometext', duration, true).then(isTrue);
+            return this.client.waitForText('.sometext', duration, true).then(checkTrue);
         });
 
     });
@@ -71,11 +71,11 @@ describe('waitFor',function() {
         beforeEach(h.setup());
 
         it('should return w/o err after element got a value', function() {
-            return this.client.waitForValue('//*[contains(@class, "waitForValueEnabledReverse")]', duration).then(isTrue);
+            return this.client.waitForValue('//*[contains(@class, "waitForValueEnabledReverse")]', duration).then(checkTrue);
         });
 
         it('(reverse) should return w/o err after element lost its value', function() {
-            return this.client.waitForValue('.waitForValueEnabled', duration, true).then(isTrue);
+            return this.client.waitForValue('.waitForValueEnabled', duration, true).then(checkTrue);
         });
 
     });
@@ -85,33 +85,40 @@ describe('waitFor',function() {
         beforeEach(h.setup());
 
         it('should return w/o err after element moved into document bounderies', function() {
-            return this.client.waitForVisible('//*[contains(@class, "notVisible")]', duration).then(isTrue);
+            return this.client.waitForVisible('//*[contains(@class, "notVisible")]', duration).then(checkTrue);
         });
 
         it('(reverse) should return w/o err after element left document bounderies', function() {
-            return this.client.waitForVisible('.onMyWay', duration, true).then(isTrue);
+            return this.client.waitForVisible('.onMyWay', duration, true).then(checkTrue);
         });
 
         // TODO replicate https://github.com/webdriverio/webdriverio/issues/434
         it('should return w/o err after element is created', function() {
             var e = '.lateElem';
             return this.client
-                .isExisting(e).then(isFalse)
-                .waitForVisible(e, duration).then(isTrue);
+                .isExisting(e).then(checkFalse)
+                .waitForVisible(e, duration).then(checkTrue); // TODO fails under phantomjs but not chrome. `grunt test-functional --spec=test/spec/waitFor.js`
         });
 
         it('should return w/o err after element becomes visible', function() {
             var e = '.notVisible';
             return this.client
-                .isVisible(e).then(isFalse)
-                .waitForVisible(e, duration).then(isTrue);
+                .isVisible(e).then(checkFalse)
+                .waitForVisible(e, duration).then(checkTrue);
         });
 
-        it('should return w/o err after element becomes invisible', function() {
+        it('should return w/o err after element becomes displayed', function() {
+            var e = '.notDisplayable';
+            return this.client
+                .isVisible(e).then(checkFalse)
+                .waitForVisible(e, duration).then(checkTrue);
+        });
+
+        it('(reverse) should return w/o err after element becomes invisible', function() {
             var e = '.onMyWay';
             return this.client
-                .isVisible(e).then(isTrue)
-                .waitForVisible(e, duration, true).then(isTrue);
+                .isVisible(e).then(checkTrue)
+                .waitForVisible(e, duration, true).then(checkTrue);
         });
 
     });
